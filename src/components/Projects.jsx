fc123feae4638b269602985c78d28b95702286e7
unknown <vsagar267@gmai;.com>
2023-02-20 23:19:25 +0530

    projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const Work = () => {
   
   return (
     <div className="flex flex-col w-full ml-[110px]">
-      <h1 className="text-[150px] font-bold projects">projects</h1>
+      
       <div className="bg-[#ffffff] boxshadow2 rounded-lg mt-16 ml-[310px] px-5 py-2 w-[745px]">
         <ul className="list-none flex flex-row space-x-8 font-[Poppins] text-[17px] cursor-pointer ">
           <li onClick={()=>setItems(project)}>All Projects</li>
@@ -29,14 +29,15 @@ const Work = () => {
           <li onClick={()=>filterItem("fullstack")}>Full Stack(MERN)</li>
         </ul>
       </div>
-      <div className="h-[400px] mt-16 ml-20 w-[1225px] boxshadow2 rounded-lg flex flex-row gap-8 items-center w-[100vw] justify-center projectdiv px-4">
+      <div className=" mt-16 ml-20 w-[1225px] boxshadow2 rounded-lg flex flex-row gap-8 items-center w-[100vw] justify-center projectdiv px-6 py-8 mb-8">
         {items.map((project, index)=>(
-          <div className="bg-[#404040] px-6 py-3 rounded-lg" key={project.id}>
+          <div className="bg-[#404040] px-6 py-3 rounded-lg w-[220px]" key={project.id}>
             <div className="h-[250px]">{project.img}</div>
             <h2 className="text-[20px] text-white">{project.title}</h2>
           </div>
         ))}
       </div>
+      <h1 className="text-[150px] font-bold projects">projects</h1>
     </div>
   );
 };
--- src/constant/index.js
+++ src/constant/index.js
@@ -35,13 +35,15 @@ export const nav = [{
 ]
 
 export const project =[
+
+    // HTML CSS Projects
     {
         id: "1",
         title: "Paytm UI Clone",
         category: "htmlcss",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/Paytm_Clone",
+        deployed : "https://paytm-clone-html-tailwind.netlify.app/",
     },
 
     {
@@ -49,54 +51,142 @@ export const project =[
         title: "Shopify UI Clone",
         category: "htmlcss",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/Shopify-clone",
+        deployed : "https://shopify-clone-vs.netlify.app/",
     },
 
     {
         id: "3",
-        title: "Rhode UI Clone",
+        title: "Rode UI Clone",
         category: "htmlcss",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/Rode",
+        deployed : "https://rode-clone-vs.netlify.app/",
     },
 
     {
         id: "4",
-        title: "Calculator",
-        category: "javascript",
+        title: "Product Design Landing Page",
+        category: "htmlcss",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/project-15-product-design-landingpage",
+        deployed : "https://product-design-landing-page-vs.netlify.app/s",
     },
 
     {
         id: "5",
-        title: "Modern Website",
-        category: "react",
+        title: "Business Landing Page",
+        category: "htmlcss",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/project-12-business-landing-page",
+        deployed : "https://business-landing-page-vs.netlify.app/",
     },
 
     {
         id: "6",
-        title: "Backend",
-        category: "backend",
+        title: "Hosting Landing Page",
+        category: "htmlcss",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/project-11-hosting-landing-page",
+        deployed : "https://hosting-landing-page-vs.netlify.app/",
     },
 
     {
         id: "7",
-        title: "MERN  MERN MERN vMERN",
+        title: "Happy Diwali",
+        category: "htmlcss",
+        img: "",
+        git: "https://github.com/vsagar26/Happy-Diwali",
+        deployed : "https://happy-diwali-vs.netlify.app/",
+    },
+
+     // Javascript Projects
+
+
+     {
+        id: "8",
+        title: "QR Code Generator",
+        category: "javascript",
+        img: "",
+        git: "https://github.com/vsagar26/07_qr-code-generator-js",
+        deployed : "https://qr-code-generator-js-vs.netlify.app/",
+    },
+
+    {
+        id: "9",
+        title: "Word & Letter Counter",
+        category: "javascript",
+        img: "",
+        git: "https://github.com/vsagar26/word-letter-counter-js",
+        deployed : "https://word-letter-counter-js.netlify.app/",
+    },
+
+    {
+        id: "10",
+        title: "Gradient Background Changer",
+        category: "javascript",
+        img: "",
+        git: "https://github.com/vsagar26/gradient-background-changer",
+        deployed : "https://gradient-background-changer-js.netlify.app/",
+    },
+
+     {
+        id: "11",
+        title: "Todo Task",
+        category: "javascript",
+        img: "",
+        git: "https://github.com/vsagar26/todo-task-js",
+        deployed : "https://todo-task-js.netlify.app/",
+    },
+
+    // React Projects
+
+    {
+        id: "12",
+        title: "Modern Bank Landing Page (from YT Video)",
+        category: "react",
+        img: "",
+        git: "https://github.com/vsagar26/bank-website-ytclone",
+        deployed : "https://bank-website-ytclone.netlify.app/",
+    },
+
+    {
+        id: "13",
+        title: "Random Password Generator",
+        category: "react",
+        img: "",
+        git: "https://github.com/vsagar26/random-password-generator-react",
+        deployed : "https://random-password-generator-vs.netlify.app/",
+    },
+
+    {
+        id: "14",
+        title: "Weather App",
+        category: "react",
+        img: "",
+        git: "https://github.com/vsagar26/weather-app-react",
+        deployed : "https://weather-react-app-js.netlify.app/",
+    },
+
+    {
+        id: "15",
+        title: "Backend ",
+        category: "backend",
+        img: "",
+        git: "https://github.com/vsagar26/weather-app-react",
+        deployed : "https://weather-react-app-js.netlify.app/",
+    },
+
+    {
+        id: "16",
+        title: "Full Stack ",
         category: "fullstack",
         img: "",
-        git: "",
-        deployed : "",
+        git: "https://github.com/vsagar26/weather-app-react",
+        deployed : "https://weather-react-app-js.netlify.app/",
     },
+    
+    
     
 ];
 
